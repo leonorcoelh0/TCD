d1c382c847df6ce8e42faad636bda8fdc83372cd
Leonor Coelho <leonor.coelho@gmail.com>
2020-10-13 18:20:18 +0100

    boxplot

--- Code/mainActivity.py
+++ Code/mainActivity.py
@@ -6,16 +6,18 @@ dir_files = "../Dataset/"
 def readFiles():
     part = dict()
     for i in range(1):
-        print("Ler individuo",i)
-        part[i] = np.empty([1,1])
+        part[i] = None
         for d in range(1,6):
             file_name = dir_files + "part" + str(i) + "/part" + str(i) + "dev" + str(d) + ".csv"
-            print("Ler dev",d)
             csv_file = open(file_name)
             csv_reader = csv.reader(csv_file, delimiter=',')
-            # print (csv_reader)
-            rows = np.array(list(csv_reader))
-            print(rows.shape)
+            rows = np.array(list(csv_reader)).astype(np.float)
+            if(part[i] == None):
+                part[i] = np.array(rows)
+            else:
+                np.append(part[i],rows)
+
+        print(part[i].shape)
 
 
 readFiles()
